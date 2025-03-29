acae994566ffcf9c45ae78fa984a482b371a7407
Derrick Ratnaharan <derrick3309@yahoo.com>
2025-03-29 01:41:36 -0400

    styling

--- context/userContext.js
+++ context/userContext.js
@@ -23,7 +23,11 @@ export default function UserContextComp({ children }) {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ idToken }),
-          }).then(res => res.json()).then(data => {document.cookie = "sessionCookie="+data.sessionCookie});
+          }).then(res => res.json()).then(data => {
+            if (data.sessionCookie) {
+              document.cookie = "sessionCookie="+data.sessionCookie
+            }
+          });
           var isPremium = false
           const newPremiumStatus = user
           ? await getPremiumStatus(firebaseApp)

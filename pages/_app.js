c272362162132eb41b267d7b2ad7b4e0655acf98
Derrick Ratnaharan <derrick3309@yahoo.com>
2025-04-04 12:00:04 -0400

    added a sell cooldown

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import UserProvider from "../context/userContext";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
+      <SpeedInsights/>
       <Component {...pageProps} />
     </UserProvider>
   );

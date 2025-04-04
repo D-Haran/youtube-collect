4f1af8719415ae50d7d44336ff3a9b9c4e6fd398
Derrick Ratnaharan <derrick3309@yahoo.com>
2025-04-04 12:23:36 -0400

    added a sell cooldown

--- pages/api/user/[userId]/sell_video_investment/route.js
+++ pages/api/user/[userId]/sell_video_investment/route.js
@@ -7,7 +7,7 @@ export default async function POST(req, res) {
     // const { userId } = req.params;
     const { investment_id, userId, profited, viewsAtSell } = req.body;
     function getSellCooldownHours(roiMult, percent_of_balance) {
-        if (roiMult <= 1.5 || percent_of_balance <= 0.1) return 0;
+        if (roiMult <= 1.25 || percent_of_balance <= 0.1) return 0;
         const adjustedMult = roiMult * percent_of_balance; // scale ROI by significance
         const maxCooldown = 12;
         const scaled = Math.log2(adjustedMult - 0.5);
@@ -38,7 +38,7 @@ export default async function POST(req, res) {
         historyInvestment.dateOfActivity = new Date(Date.now())
         const roiMult = 1 + (profited / sellingInvestment.investment_total)
         var on_cooldown = cooldown_from_firestore ? (new Date(cooldown_from_firestore.seconds*1000)) > Date.now() : false
-        if (!on_cooldown) {
+        if (!on_cooldown || roiMult <= 1.25) {
             const percent_of_balance = (sellingInvestment.percent_of_balance / 100) || 0.05
             const cooldownHours = getSellCooldownHours(roiMult, percent_of_balance);
         const cooldownMs = cooldownHours * 60 * 60 * 1000;

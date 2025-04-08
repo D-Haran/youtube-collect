16ba95d4a70bdde2571a6c847f76f7da208e5994
Derrick Ratnaharan <derrick3309@yahoo.com>
2025-04-08 01:17:11 -0400

    styling

--- components/historyInvestment/historyInvestment.js
+++ components/historyInvestment/historyInvestment.js
@@ -1,15 +1,27 @@
 import React, { Fragment } from 'react'
 import styles from './historyInvestment.module.css'
+import {numify} from "numify"
 
 const HistoryInvestment = ({video}) => {
   return (
     <div className={styles.investmentContainer}>
+      <div className={styles.typeContainer}>
         <h3 className={styles.investmentType} style={video.investmentType == "BUY" ? {color: 'green'}: {color: 'red'}}>{video?.investmentType}</h3>
-      <div className={styles.topVideoStat}>
+      {
+          video.profit &&
+          <div className={styles.ratioStat} style={video?.profit >= 0 ? {color: 'green'} : {color: 'red'}}>
+          <h3>{video?.profit >= 0 ? "+" : "-"}{video?.profit ? (numify(Number(video.profit.toFixed(2))) || "<0.00001") || "N/A" : "..."}</h3>
+        </div>
+        }
+      </div>
+        <div className={styles.topVideoStat}>
         <div className={styles.titleAndThumbnail} onClick={() => window.open(`https://youtube.com/watch?v=${video?.video_metadata?.id}`)}>
           <img src={video?.video_metadata?.snippet?.thumbnails?.default?.url} className={styles.investedThumbnail}/>
         <h3  className={styles.investedVideoTitle}>{video?.video_metadata?.snippet?.title}</h3>
+        
         </div>
+        
+        
         </div>
       
       <div className={styles.bottomVideoStat}>

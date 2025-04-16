38c15e2b97a98da7f98a9235a2966fd38a9544f5
Derrick Ratnaharan <derrick3309@yahoo.com>
2025-04-16 18:32:58 -0400

    new usernames

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import UserProvider from "../context/userContext";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
+
 
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }) {

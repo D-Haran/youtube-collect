c284f16a2cfbdfaea07b8949dd84444834870579
Derrick Ratnaharan <derrick3309@yahoo.com>
2025-04-16 18:04:58 -0400

    new usernames

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
     await fetch(`/api/user/${userId}/set_data/route`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ balance: 100, userId: userId, investments: videoInvestments, userName: generateUsername() })
+        body: JSON.stringify({ balance: 100, userId: userId, investments: videoInvestments, userName: generateUsername("", 2, 15) })
     })
     .then(res => res.json())
   }

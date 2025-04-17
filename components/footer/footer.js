23c6162d02f39851ada0029c8ff9d75c365b2714
Derrick Ratnaharan <derrick3309@yahoo.com>
2025-04-17 15:24:25 -0400

    styling

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -10,17 +10,15 @@ const Footer = () => {
           <div className={styles.logoGroup}>
           <Image 
           src="/youCoinsLogo.png"
-        width={100}
-        height={100}
+        width={60}
+        height={60}
         alt="Picture of the author"
         />
             <span className={styles.brand}>Youtube Collect</span>
           </div>
 
           <div className={styles.links}>
-            <a href="#" className={styles.link}>Terms</a>
-            <a href="#" className={styles.link}>Privacy</a>
-            <a href="#" className={styles.link}>Contact</a>
+            <a href="mailto:ytcollectgame@gmail.com" className={styles.link}>Contact Me</a>
           </div>
 
           <div className={styles.copyright}>
--- pages/about.js
+++ pages/about.js
@@ -10,8 +10,25 @@ import { numify } from 'numify';
 import Link from 'next/link'
 import Head from 'next/head';
 
+import { useCallback } from "react";
+import Particles from "react-particles";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+
 const Index = () => {
     const [totalUsers, setTotalUsers] = useState(null)
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
     async function firestoreGetTotalUsers() {
         await fetch(`/api/leaderboard/total_users/route`, {
             method: "GET"
@@ -35,8 +52,19 @@ const Index = () => {
         <meta name="google-site-verification" content="odCTNV3mrGrvM2JpHbiMllYzHWTEldh95mlprg9skOA" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.heroSection}>
+      
+        <p className={styles.alreadyAPlayer}>
+            Already a player?
+            <Link href={`/`} passHref legacyBehavior>
+            <button className={styles.dashboardButton}>
+                Go To Dashboard
+            </button>
+            </Link>
+        </p>
+        <div className={styles.heroSection} style={{zIndex:100}}>
+            
           <div className={styles.heroContent}>
+            
           <Image 
           src="/youCoinsLogo.png"
         width={100}
@@ -56,12 +84,108 @@ const Index = () => {
             <button className={styles.ctaButton}>Install Chrome Extension</button>
             </Link>
           </div>
+          
         </div>
+        <Particles
+            init={particlesInit}
+            loaded={particlesLoaded}
+            className={styles.particles}
+            height={'100%'}
+            options={{
+                background: {
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#c10000",
+                    },
+                    links: {
+                        color: "#000000",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 2,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 0.2,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 5, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+        <div style={{zIndex:10, position: "sticky"}}>
 
-        <GameIntro />
+           <GameIntro />
+        <div><div className={styles.demoVideo}>
+          <h1>Demo</h1>
+          <div
+            style={{
+                position: "relative", 
+                overflow: "hidden",
+                display: "flex",
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+            >
+                
+                    <iframe
+                src="https://www.loom.com/embed/2734d7eeaa904f5d92ebc3f6438fb053?sid=da23bc8a-864b-4cde-be4e-395d03d68585"
+                frameBorder="0"
+                
+                allowFullScreen
+                style={{
+                height: "100%",
+                }}
+            ></iframe>
+                </div>
+            
+            </div>
+        </div>
         <HowToPlay />
         <GameTips />
-        <Footer />
+        <Footer /> 
+        </div>
+        
       </main>
 
       {/* <Footer /> */}

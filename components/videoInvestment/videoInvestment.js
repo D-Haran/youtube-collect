6a57bab2bd45769b507597f442326e3ae87471b1
Derrick Ratnaharan <derrick3309@yahoo.com>
2025-04-26 11:04:20 -0400

    styling

--- components/videoInvestment/videoInvestment.js
+++ components/videoInvestment/videoInvestment.js
@@ -71,10 +71,12 @@ const VideoInvestment = ({video, me}) => {
                   <b><p>{video.lastMilestone > 0 && "Dodged a meltdown at "+((Number(video.lastMilestone) / 100))+"x returns 🔥"}</p></b>
                 }
                 {
-                 ((video?.profit_percent*100) > video?.lastMilestone && nextMilestoneRange >= 70) 
+                 true
                 &&
                 <b><p style={video?.videoAngelInvestor && {paddingBottom: '20px'}}>
-                "Your position is overheating... there's a {`${(getCrashChanceForMilestone((Number(video.lastMilestone)+100))*100).toFixed(0)}%` || ""} chance of a meltdown at {(Number(video.lastMilestone)+100)}% returns!"
+                Your position is overheating {' '}
+                <img src="/warning.svg" height={"15px"} />{' '}
+                There's a {`${(getCrashChanceForMilestone((Number(video.lastMilestone)+100))*100).toFixed(0)}%` || ""} chance of a meltdown at {(Number(video.lastMilestone)+100)}% returns!
                 </p></b>
 }
               </div>

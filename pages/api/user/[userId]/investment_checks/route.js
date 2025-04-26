745b47d3c89d1260046e7f0758e89d5e94b7978e
Derrick Ratnaharan <derrick3309@yahoo.com>
2025-04-26 10:39:44 -0400

    styling

--- pages/api/user/[userId]/investment_checks/route.js
+++ pages/api/user/[userId]/investment_checks/route.js
@@ -133,7 +133,7 @@ export default async function GET(req, res) {
                       var minutes = Math.floor((diff/1000)/60);
                       console.log("minutes: ", minutes)
                       const openingChance = Math.random()
-                    if (openingChance < 0.01 && minutes >= 10 && data.balance >= 900) {
+                    if (openingChance < 0.001 && minutes >= 10 && data.balance >= 900) {
                       console.log(`💥 CRASH! Investment crashed at ${profits}% return!`);
                 
                       // Apply the crash (e.g., halve the current ratio)
